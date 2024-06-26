0a64bd21183c97b2f0705c1496633075227c3e7f
J-coder118 <nanoweb.7jw@gmail.com>
2024-06-26 04:00:34 -0300

    feat: complete

--- train/train.py
+++ train/train.py
@@ -1,236 +1,112 @@
-import torch, platform
-from datasets import load_dataset, Dataset
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    BitsAndBytesConfig,
-    HfArgumentParser,
-    TrainingArguments,
-    pipeline,
-    logging,
-)
-from peft import LoraConfig, PeftModel
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, TrainingArguments
+from datasets import load_dataset
+from random import randrange
 from trl import SFTTrainer
-import pandas as pd
+from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model
 
-def create_prompt(row):
-    # video check
-    prompt = f" Context: {row[4]}\nInstruction: {row[3]}\nResponse: {row[5]}"
-    return prompt
-
+use_flash_attention = False
 # The model that you want to train from the Hugging Face hub
-model_name = "NousResearch/Llama-2-7b-chat-hf"
+model_id = "NousResearch/Llama-2-7b-chat-hf"
 
 # The instruction dataset to use
 dataset = []
 # Load the CSV file into a Pandas DataFrame
-df = pd.read_csv('ai_logs.csv', header=None)
+# df = pd.read_csv('ai_logs.csv')
+dataset = load_dataset("csv", data_files="ai_logs.csv")
 
+columns_to_keep = ['instruction', 'prompt', 'response']
+
+# Remove the unnecessary columns
+dataset = dataset['train'].remove_columns([col for col in dataset['train'].features if col not in columns_to_keep])
+
+print(f"dataset size: {len(dataset)}")
+print(dataset[randrange(len(dataset))])
+# dataset size: 15011
 # Fine-tuned model name
-new_model = "llama-2-7b-finetuned"
+new_model = "llama-2-7b-finetuned-4"
 
-################################################################################
-# QLoRA parameters
-################################################################################
+def format_dataset(row):
+  return f"prompt: {row['prompt']}, response: {row['response']}"
 
-# LoRA attention dimension
-lora_r = 64
+## Testing the format
+print(format_dataset(dataset[randrange(len(dataset))]))
 
-# Alpha parameter for LoRA scaling
-lora_alpha = 16
 
-# Dropout probability for LoRA layers
-lora_dropout = 0.1
-
-################################################################################
-# bitsandbytes parameters
-################################################################################
-
-# Activate 4-bit precision base model loading
-use_4bit = True
-
-# Compute dtype for 4-bit base models
-bnb_4bit_compute_dtype = "float16"
-
-# Quantization type (fp4 or nf4)
-bnb_4bit_quant_type = "nf4"
-
-# Activate nested quantization for 4-bit base models (double quantization)
-use_nested_quant = False
-
-################################################################################
-# TrainingArguments parameters
-################################################################################
-
-# Output directory where the model predictions and checkpoints will be stored
-output_dir = "./results"
-
-# Number of training epochs
-num_train_epochs = 1
-
-# Enable fp16/bf16 training (set bf16 to True with an A100)
-fp16 = False
-bf16 = False
-
-# Batch size per GPU for training
-per_device_train_batch_size = 4
-
-# Batch size per GPU for evaluation
-per_device_eval_batch_size = 4
-
-# Number of update steps to accumulate the gradients for
-gradient_accumulation_steps = 1
-
-# Enable gradient checkpointing
-gradient_checkpointing = True
-
-# Maximum gradient normal (gradient clipping)
-max_grad_norm = 0.3
-
-# Initial learning rate (AdamW optimizer)
-learning_rate = 2e-4
-
-# Weight decay to apply to all layers except bias/LayerNorm weights
-weight_decay = 0.001
-
-# Optimizer to use
-optim = "paged_adamw_32bit"
-
-# Learning rate schedule
-lr_scheduler_type = "cosine"#"constant"
-
-# Number of training steps (overrides num_train_epochs)
-max_steps = -1
-
-# Ratio of steps for a linear warmup (from 0 to learning rate)
-warmup_ratio = 0.0011
-
-# Group sequences into batches with same length
-# Saves memory and speeds up training considerably
-group_by_length = True
-
-# Save checkpoint every X updates steps
-save_steps = 0
-
-# Log every X updates steps
-logging_steps = 25
-
-################################################################################
-# SFT parameters
-################################################################################
-
-# Maximum sequence length to use
-max_seq_length = 250
-
-# Pack multiple short examples in the same input sequence to increase efficiency
-packing = False
-
-# Load the entire model on the GPU 0
-device_map = {"": 0}
-
-train_df = df
-
-train_df['text'] = train_df.apply(create_prompt, axis=1)
-data_df = train_df
-
-dataset = Dataset.from_pandas(data_df)
-# Load tokenizer and model with QLoRA configuration
-compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
-
+# BitsAndBytesConfig int-4 config
 bnb_config = BitsAndBytesConfig(
-    load_in_4bit=use_4bit,
-    bnb_4bit_quant_type=bnb_4bit_quant_type,
-    bnb_4bit_compute_dtype=compute_dtype,
-    bnb_4bit_use_double_quant=use_nested_quant,
+    load_in_4bit=True, bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16
 )
-# Check GPU compatibility with bfloat16
-if compute_dtype == torch.float16 and use_4bit:
-    major, _ = torch.cuda.get_device_capability()
-    if major >= 8:
-        print("=" * 80)
-        print("Your GPU supports bfloat16: accelerate training with bf16=True")
-        print("=" * 80)
 
-# Load base model
+# Load model and tokenizer
 model = AutoModelForCausalLM.from_pretrained(
-    model_name,
+    model_id,
     quantization_config=bnb_config,
-    device_map=device_map
+    use_cache=False,
+    use_flash_attention_2=use_flash_attention,
+    device_map="auto",
 )
-model.config.use_cache = False
 model.config.pretraining_tp = 1
 
-# Load LLaMA tokenizer
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
 tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
+tokenizer.padding_side = "right"
 
-# Load LoRA configuration
+
+
+
+# LoRA config based on QLoRA paper
 peft_config = LoraConfig(
-    lora_alpha=lora_alpha,
-    lora_dropout=lora_dropout,
-    r=lora_r,
-    bias="none",
-    task_type="CAUSAL_LM",
+        lora_alpha=16,
+        lora_dropout=0.1,
+        r=64,
+        bias="none",
+        task_type="CAUSAL_LM",
 )
 
-# Set training parameters
-training_arguments = TrainingArguments(
-    output_dir=output_dir,
-    num_train_epochs=num_train_epochs,
-    per_device_train_batch_size=per_device_train_batch_size,
-    gradient_accumulation_steps=gradient_accumulation_steps,
-    optim=optim,
-    save_steps=save_steps,
-    logging_steps=logging_steps,
-    learning_rate=learning_rate,
-    weight_decay=weight_decay,
-    fp16=fp16,
-    bf16=bf16,
-    max_grad_norm=max_grad_norm,
-    max_steps=max_steps,
-    warmup_ratio=warmup_ratio,
-    group_by_length=group_by_length,
-    lr_scheduler_type=lr_scheduler_type,
-    report_to="tensorboard"
+
+# prepare model for training
+model = prepare_model_for_kbit_training(model)
+model = get_peft_model(model, peft_config)
+
+
+
+
+args = TrainingArguments(
+    output_dir="llama-7-int4-dolly",
+    num_train_epochs=3,
+    per_device_train_batch_size=6 if use_flash_attention else 4,
+    gradient_accumulation_steps=2,
+    gradient_checkpointing=True,
+    optim="paged_adamw_32bit",
+    logging_steps=10,
+    save_strategy="epoch",
+    learning_rate=2e-4,
+    bf16=True,
+    tf32=True,
+    max_grad_norm=0.3,
+    warmup_ratio=0.03,
+    lr_scheduler_type="constant",
+    disable_tqdm=True # disable tqdm since with packing values are in correct
 )
 
-# Set supervised fine-tuning parameters
+max_seq_length = 2048 # max sequence length for model and packing of the dataset
+
 trainer = SFTTrainer(
     model=model,
     train_dataset=dataset,
     peft_config=peft_config,
-    dataset_text_field="text",
     max_seq_length=max_seq_length,
     tokenizer=tokenizer,
-    args=training_arguments,
-    packing=packing,
+    packing=True,
+    formatting_func=format_dataset,
+    args=args,
 )
-# Train model
-trainer.train()
-# Save trained model
-trainer.model.save_pretrained(new_model)
 
 
+# train
+trainer.train() # there will not be a progress bar since tqdm is disabled
 
-# from huggingface_hub import notebook_login
-
-# notebook_login()
-# Reload model in FP16 and merge it with LoRA weights
-base_model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    low_cpu_mem_usage=True,
-    return_dict=True,
-    torch_dtype=torch.float16,
-    device_map=device_map,
-)
-model = PeftModel.from_pretrained(base_model, new_model)
-model = model.merge_and_unload()
-
-# Reload tokenizer to save it
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right"
-
-model.push_to_hub(new_model, use_temp_dir=False)
-tokenizer.push_to_hub(new_model, use_temp_dir=False)
+# save model
+trainer.save_model()
